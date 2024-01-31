60afa26acbab5443d5b7fb5f35c8d80e266acd1d
Viorel Soltan <joomjoom2011@gmail.com>
2024-01-31 20:12:14 +0000

    add g

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
-  modules: ['nuxt-icon', '@nuxt/ui', 'nuxt-swiper'],
+  modules: ['nuxt-icon', '@nuxt/ui', 'nuxt-swiper', 'nuxt-gtag'],
 
   postcss: {
     plugins: {
